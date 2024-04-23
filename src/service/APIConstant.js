3e5662425aa928e0f15597a52e873a0e259d8971
Kelven Bragoa <kelvenbragoa@hotmail.com>
2024-04-23 19:32:37 +0200

    first commit

--- src/service/APIConstant.js
+++ src/service/APIConstant.js
@@ -5,8 +5,8 @@ import { useRouter } from 'vue-router';
 
 // export const baseURL = 'http://127.0.0.1:8000/api';
 // export const storageURL = 'http://127.0.0.1:8000/storage/images/';
-export const baseURL = 'https:backend.emersonmite.com/api';
-export const storageURL = 'https:backend.emersonmite.com/storage/images/';
+export const baseURL = 'https://backend.emersonmite.com/api';
+export const storageURL = 'https://backend.emersonmite.com/storage/images/';
 
 export function logout() {
 
